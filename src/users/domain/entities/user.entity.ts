1f2f8fc1b407b79f6f79c41952d63be19aeac091
Felipe <adfelipealmeida@gmail.com>
2025-02-14 14:40:39 -0300

    Configuracao inicial da UserEntity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,14 @@
-export class UserEntity {}
+// Regras cruciais de negócio, razão de ser do software
+
+export type UserProps = {
+  name: string
+  email: string
+  password: string
+  createdAt?: Date // Ao criar um usuário a pessoa não precisa informar a data de criação, ela será preenchida automaticamente
+}
+
+export class UserEntity {
+  constructor(public readonly props: UserProps) {
+    this.props.createdAt = this.props.createdAt ?? new Date()
+  }
+}
