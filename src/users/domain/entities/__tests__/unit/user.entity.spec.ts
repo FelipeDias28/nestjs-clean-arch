d41652ecf27903889bf4c980e1a679770c97e8b6
Felipe <adfelipealmeida@gmail.com>
2025-02-19 08:20:22 -0300

    Testes dos setters e dos metodos de update

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -23,6 +23,13 @@ describe('UserEntity', () => {
     expect(typeof sut.props.name).toBe('string')
   })
 
+  it('Setter of name field', () => {
+    // Colchetes é um forma de driblar a propriedade privada, somente nos testes
+    sut['name'] = 'other name' // testando somente se o setter atualiza o valor
+    expect(sut.props.name).toEqual('other name')
+    expect(typeof sut.props.name).toBe('string')
+  })
+
   it('Getter of email field', () => {
     expect(sut.props.email).toBeDefined() // Propriedade não é undefined
     expect(sut.props.email).toEqual(props.email)
@@ -35,8 +42,25 @@ describe('UserEntity', () => {
     expect(typeof sut.props.password).toBe('string')
   })
 
+  it('Setter of password field', () => {
+    // Colchetes é um forma de driblar a propriedade privada, somente nos testes
+    sut['password'] = 'other password' // testando somente se o setter atualiza o valor
+    expect(sut.props.password).toEqual('other password')
+    expect(typeof sut.props.password).toBe('string')
+  })
+
   it('Getter of createdAt field', () => {
     expect(sut.props.createdAt).toBeDefined() // Propriedade não é undefined
     expect(sut.props.createdAt).toBeInstanceOf(Date)
+  })
+
+  it('Should update a user', () => {
+    sut.update('other name')
+    expect(sut.props.name).toEqual('other name')
+  })
+
+  it('Should update the password', () => {
+    sut.updatePassword('other password')
+    expect(sut.props.password).toEqual('other password')
   })
 })
