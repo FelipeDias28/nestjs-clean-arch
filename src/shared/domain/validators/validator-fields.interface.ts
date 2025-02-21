ef0ba21f8f9fb753d21a340aec6c45e2100d9caa
Felipe <adfelipealmeida@gmail.com>
2025-02-21 08:55:21 -0300

    Adicionado a interface ValidatorFieldsInterface

--- src/shared/domain/validators/validator-fields.interface.ts
+++ src/shared/domain/validators/validator-fields.interface.ts
@@ -3,11 +3,11 @@ export type FieldsErrors = {
    *  Um campo pode ter vários erros: name = obrigatório, string, 255
    * E para cada campo uma mensagem de erro diferente
    **/
-  [field: string]: string[]
+  [field: string]: string[] | null
 }
 
 export interface ValidatorFieldsInterface<PropsValidated> {
-  errors: FieldsErrors // Tipo do retorno do erro
-  validaData: PropsValidated // Os dados recebidos
+  errors: FieldsErrors | null // Tipo do retorno do erro
+  validatedData: PropsValidated | null // Os dados recebidos
   validate(data: any): boolean // Função de validação que retorna se estão ou não validado
 }
