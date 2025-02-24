da9c651bbeac73e5bd8ca361104e564f648e1e68
Felipe <adfelipealmeida@gmail.com>
2025-02-24 07:44:08 -0300

    ClassValidateFields - Testes unitarios - teste mensagem de erro

---
+++ src/shared/domain/validators/__tests__/unit/class-validator-fields.spec.ts
@@ -0,0 +1,30 @@
+import { ClassValidatorFields } from '../../class-validator-fields'
+import * as lassValidator from 'class-validator'
+
+class StubClassValidatorFields extends ClassValidatorFields<{
+  field: string
+}> {}
+
+describe('ClassValidatorFields Unit Tests', () => {
+  it('Should initialize erros and validatedData variables with null', () => {
+    const sut = new StubClassValidatorFields()
+
+    expect(sut.errors).toBeNull()
+    expect(sut.validatedData).toBeNull()
+  })
+
+  it('Should validate with errors', () => {
+    // Criando um mock de um método
+    const spyValidateSync = jest.spyOn(lassValidator, 'validateSync')
+    spyValidateSync.mockReturnValue([
+      { property: 'field', constraints: { isRequired: 'test error' } },
+    ])
+
+    const sut = new StubClassValidatorFields()
+
+    expect(sut.validate(null)).toBeFalsy()
+    expect(spyValidateSync).toHaveBeenCalled() // O método precisa ter sido chamado
+    expect(sut.validatedData).toBeNull()
+    expect(sut.errors).toStrictEqual({ field: ['test error'] })
+  })
+})
